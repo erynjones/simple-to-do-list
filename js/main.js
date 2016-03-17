8b4f5485e084d2e16020153034ef3dc2fd32d30d
erynjones <eryn.jones16@hotmail.com>
2016-03-17 13:42:09 -0400

    Started list

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var $input = $('#to-do-input');
+var $list = $(".list");
+
+$('#to-do-list').on('submit', function (e) {
+  var $li = $('<li>');
+  e.preventDefault();
+  $li.html($input.val());
+  $list.append($li);
+});
